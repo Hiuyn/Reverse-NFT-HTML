123ab32979d85507a72b254d4b24a8057f0ef203
hienquach <106219633+dev14sinka@users.noreply.github.com>
2023-11-04 01:32:31 +0700

    Update animation and carousel

--- js/index.js
+++ js/index.js
@@ -29,6 +29,43 @@
 //   // parallax1.style.top = -value + 50 + 666 + 'px'
 //   // parallax2.style.top = -value + 50 + 769 + 'px'
 // })
+function chooseCharater(e) {
+    $('.charater.active').removeClass('active')
+    $(`#${e}`).addClass('active')
+    $(`.infor__charater_background img`).attr('src', `../image/parallax/Charater_Wall_${e}.png`)
+}
 
-var tl = gsap.timeline({duration: 1})
+var tl = gsap.timeline({ duration: 1 })
 tl.fromTo("#parallax1", { y: 200 }, { y: 0 })
+
+var tl1 = gsap.timeline({ duration: 2.5, repeat: -1, ease: "slow(0.5,0.8)" })
+tl1.to('#animation_charater', { y: 0 })
+    .to('#animation_charater', { y: -50 })
+    .to('#animation_charater', { y: 10 })
+    .to('#animation_charater', { y: 0 })
+
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    speed: 1000,
+    autoplay: {
+        delay: 3000,
+    },
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 80,
+        depth: 200,
+        modifier: 1,
+        slideShadows: false,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
